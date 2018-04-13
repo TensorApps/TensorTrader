c04cb3d2706e6063671d2afdf8fcd8ddf2fce269
Julian Christov <julian.christov@live.com>
2018-04-14 01:53:27 +0200

    Included all exchanges and markets; Added autocomplete + link between Exchange & Market drop-downs; Loading indicator for Markets

--- prototype/app/js/market-panel.js
+++ prototype/app/js/market-panel.js
@@ -4,21 +4,59 @@ Vue.component('market-panel', {
 <v-layout row wrap>
   <v-flex xs6>
     <v-card class="pa-3">
-      <v-select id="exchange" label="EXCHANGE" :items="exchanges" multi-line></v-select>
+      <v-select id="exchange" label="EXCHANGE" :items="exchanges" v-model="exchange" multi-line autocomplete></v-select>
     </v-card>
   </v-flex>
   <v-flex xs6>
     <v-card class="pa-3">
-      <v-select id="market" label="MARKET" :items="markets" multi-line></v-select>
+      <v-select id="market" label="MARKET" :items="markets" v-model="market" :loading="marketsLoading" multi-line autocomplete></v-select>
     </v-card>
   </v-flex>
 </v-layout>`,
   data() {
     return {
-      exchanges: ['Binance', 'Bittrex', 'Bitfinex', 'GDAX', 'KuCoin'],
-      markets: ['BTC/USD', 'ETH/USD', 'LTC/USD', 'NEO/USD', 'STRAT/USD'],
-      amount: 0.0,
-      price: 0.0
+      exchanges: this.getExchanges(),
+      markets: [],
+      marketsLoading: false,
+      exchange: '',
+      market: ''
     };
+  },
+  methods: {
+    getExchanges: function() {
+      let exchanges = [];
+      for (let exchangeId of ccxt.exchanges) {
+        let exchange = new ccxt[exchangeId]();
+        exchanges.push({ text: exchange.name, value: exchange.id });
+      }
+      //console.log(exchanges);
+      return exchanges;
+      /*
+      return [
+        { text: 'Binance', value: 'binance' },
+        { text: 'Bittrex', value: 'bittrex' },
+        { text: 'Bitfinex', value: 'bitfinex' },
+        { text: 'BitMex', value: 'bitmex' },
+        { text: 'BitStamp', value: 'bitstamp' },
+        { text: 'GDAX', value: 'gdax' },
+        { text: 'KuCoin', value: 'kucoin' }
+      ];
+      */
+    },
+    getMarkets: async function(exchangeId) {
+      this.marketsLoading = true;
+      let exchange = new ccxt[exchangeId]();
+      exchange.proxy = 'https://cors-anywhere.herokuapp.com/';
+      await exchange.loadMarkets();
+      let markets = Object.keys(await exchange.markets).sort();
+      //console.log(markets);
+      this.marketsLoading = false;
+      return markets;
+    }
+  },
+  watch: {
+    exchange: function(exchangeId) {
+      this.getMarkets(exchangeId).then(markets => (this.markets = markets));
+    }
   }
 });
